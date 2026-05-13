454637b2c3472037e68d8c014323259011cb7eb6
alpa26 <panovalecsander717@gmail.com>
2026-05-13 23:46:32 +0500

    ver 1.01.4

--- alerting.py
+++ alerting.py
@@ -1,7 +1,12 @@
 import requests
+import smtplib
 
 TG_TOKEN = "YOUR_TOKEN"
 TG_CHAT_ID = "YOUR_CHAT_ID"
+
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+smtpObj.starttls()
+smtpObj.login('justkiddingboat@gmail.com', 'just123kidding')
 
 def send_telegram(message):
     url = f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage"
@@ -11,8 +16,5 @@ def send_telegram(message):
     })
 
 def send_email(message):
-    url = f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage"
-    requests.post(url, data={
-        "chat_id": TG_CHAT_ID,
-        "text": message
-    })
+    smtpObj.sendmail("justkiddingboat@gmail.com", "michael.byrne@vice.com", message)
+    smtpObj.quit()
